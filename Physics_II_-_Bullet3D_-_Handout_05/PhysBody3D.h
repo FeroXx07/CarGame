c499280dca3719224c92843c188ff2e96b82993f
FeroXx07 <ali.shahid.1212@gmail.com>
2021-01-08 11:18:05 +0100

    Sensors

--- Physics_II_-_Bullet3D_-_Handout_05/PhysBody3D.h
+++ Physics_II_-_Bullet3D_-_Handout_05/PhysBody3D.h
@@ -27,12 +27,13 @@ public:
 	void GetTransform(float* matrix) const;
 	void SetTransform(const float* matrix) const;
 	void SetPos(float x, float y, float z);
-
+	void PhysBody3D::SetAsSensor(bool is_sensor);
 private:
 	btRigidBody* body = nullptr;
 	Primitive* parentPrimitive;
 public:
 	p2List<Module*> collision_listeners;
+	bool is_sensor = false;
 };
 
 #endif // __PhysBody3D_H__
